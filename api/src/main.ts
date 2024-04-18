0060134ffcc9a02b2fb47b2e7f0f9aee956d6683
Mateus Sarmento <omateusmento@gmail.com>
2024-04-18 11:31:05 -0300

    Use global validation pipe

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      transformOptions: {
+        enableCircularCheck: true,
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 
