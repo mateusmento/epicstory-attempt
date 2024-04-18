4b1c71267e8d502e1897f6074f1db2deca1945c4
Mateus Sarmento <omateusmento@gmail.com>
2024-04-18 12:01:42 -0300

    Add create function to objects lib

--- api/src/lib/objects.ts
+++ api/src/lib/objects.ts
@@ -1,8 +1,16 @@
-import { plainToClassFromExist } from 'class-transformer';
+import {
+  ClassConstructor,
+  plainToClass,
+  plainToClassFromExist,
+} from 'class-transformer';
 
 export function patch<T>(value: T, partial: Partial<T>) {
   return plainToClassFromExist(value, partial, {
     enableCircularCheck: true,
     enableImplicitConversion: true,
   });
+}
+
+export function create<T>(type: ClassConstructor<T>, data: Partial<T>) {
+  return plainToClass(type, data);
 }
