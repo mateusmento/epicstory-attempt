840fd0f746f41d110ac7044b6d6a01d5e98f409a
Mateus Sarmento <omateusmento@gmail.com>
2024-04-19 18:16:19 -0300

    Create vue 3 project for epicstory-app

---
+++ app/src/app/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from '@/components/HelloWorld.vue';
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="Hello world" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/foos">Foos</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ app/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <div class="greetings">
+    {{ msg }}
+  </div>
+</template>
+
+<style scoped></style>
