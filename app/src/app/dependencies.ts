a2f09e35771a2c69e0bef8705cd02871fb3d00e1
Mateus Sarmento <omateusmento@gmail.com>
2024-04-19 22:09:56 -0300

    Include foo demo

--- app/src/app/dependencies.ts
+++ app/src/app/dependencies.ts
@@ -1,8 +1,10 @@
 import { Axios } from 'axios';
 import { createAxios } from '@/core/axios';
 import { createDependencyContainer } from '@/core/dependency-injection';
+import { FooService } from '@/services/foo.service';
 
 export default createDependencyContainer((container) => {
   const axios = createAxios({ baseURL: 'http://localhost:3000' });
   container.registerInstance(Axios, axios);
+  container.registerSingleton(FooService);
 });
