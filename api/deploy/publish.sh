8453fd301b8a057be18dc427dc7e89f32fe5d42d
Mateus Sarmento <omateusmento@gmail.com>
2024-04-22 11:47:20 -0300

    Remove versions from publish.sh command

--- api/deploy/publish.sh
+++ api/deploy/publish.sh
@@ -8,6 +8,6 @@ aws ecr create-repository --repository-name epicstory-api --region sa-east-1
 aws ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin 429249706241.dkr.ecr.sa-east-1.amazonaws.com
 
 # Build and publish docker image
-docker build -t epicstory-api:0.1.0 .
-docker tag epicstory-api:0.1.0 429249706241.dkr.ecr.sa-east-1.amazonaws.com/epicstory-api
+docker build -t epicstory-api .
+docker tag epicstory-api 429249706241.dkr.ecr.sa-east-1.amazonaws.com/epicstory-api
 docker push 429249706241.dkr.ecr.sa-east-1.amazonaws.com/epicstory-api
