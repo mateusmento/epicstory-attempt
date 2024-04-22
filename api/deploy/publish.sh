c694ed9bde0f64655953211e6fcb72cb53333dcf
Mateus Sarmento <omateusmento@gmail.com>
2024-04-22 10:40:42 -0300

    Add ci/cd workflow

--- api/deploy/publish.sh
+++ api/deploy/publish.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Installing dependencies
-yum update -y
-yum install git -y
-
-# Building docker image
-git clone https://github.com/mateusmento/epicstory
-cd epicstory/api
-git checkout infra/api-deployment
-docker build -t epicstory-api:0.1.0 .
 
 # Creating AWS ECR repository
 aws ecr create-repository --repository-name epicstory-api --region sa-east-1
 
-# Publishing docker image
+# Login docker to AWS ECR
 aws ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin 429249706241.dkr.ecr.sa-east-1.amazonaws.com
+
+# Build and publish docker image
+docker build -t epicstory-api:0.1.0 .
 docker tag epicstory-api:0.1.0 429249706241.dkr.ecr.sa-east-1.amazonaws.com/epicstory-api
 docker push 429249706241.dkr.ecr.sa-east-1.amazonaws.com/epicstory-api
