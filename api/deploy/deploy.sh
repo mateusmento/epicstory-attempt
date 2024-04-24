f289e75af4f97e8437ca918f759ed0b836c48db8
Mateus Sarmento <omateusmento@gmail.com>
2024-04-24 09:49:19 -0300

    Waits for app instance to launch to retrieve its public dns when deploying api

--- api/deploy/deploy.sh
+++ api/deploy/deploy.sh
@@ -15,7 +15,19 @@ aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 aws configure set region ${AWS_REGION}
 
+# Wait for epicstory-app instance to run
+aws ec2 wait instance-exists \
+  --filters "Name=tag:Name,Values=epicstory-app" \
+  --filters "Name=instance-state-name,Values=running"
+
+export APP_HOSTNAME=$(aws ec2 describe-instances \
+  --output text \
+  --query Reservations[*].Instances[*].PublicDnsName \
+  --filters "Name=tag:Name,Values=epicstory-app")
+
+export CORS_ORIGINS="http://$APP_HOSTNAME"
+
 # Run application
 echo "Run application"
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com/epicstory-api
-docker run -it -d -p 80:3000 ${AWS_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com/epicstory-api
+docker run -it -d -p 80:3000 -e CORS_ORIGINS=$CORS_ORIGINS ${AWS_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com/epicstory-api
