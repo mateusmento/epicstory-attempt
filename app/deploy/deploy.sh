ea5e26f8f905b0ac1a95872cd4fcb6d617a5f60b
Mateus Sarmento <omateusmento@gmail.com>
2024-04-24 09:49:45 -0300

    Waits for api instance to launch to build app docker image with api public dns

--- app/deploy/deploy.sh
+++ app/deploy/deploy.sh
@@ -3,6 +3,7 @@
 # Install dependencies
 echo "Install dependencies"
 yum update -y
+yum install git -y
 yum install docker -y
 systemctl start docker
 systemctl enable docker
@@ -15,7 +16,27 @@ aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 aws configure set region ${AWS_REGION}
 
+# Wait for epicstory-api instance to run
+echo "Waiting to retreive epicstory-api ec2 instance public dns"
+
+aws ec2 wait instance-exists \
+  --filters "Name=tag:Name,Values=epicstory-api" \
+  --filters "Name=instance-state-name,Values=running"
+
+export API_HOSTNAME=$(aws ec2 describe-instances \
+  --output text \
+  --query Reservations[*].Instances[*].PublicDnsName \
+  --filters "Name=tag:Name,Values=epicstory-api")
+
+export VITE_API_URL="http://$API_HOSTNAME"
+
+# Build image
+echo "Building docker image"
+git clone https://github.com/mateusmento/epicstory
+cd epicstory/app
+git checkout infra/cicd
+docker build -t epicstory-app --build-arg VITE_API_URL=$VITE_API_URL .
+
 # Run application
 echo "Run application"
-aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com/epicstory-app
-docker run -it -d -p 80:80 ${AWS_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com/epicstory-app
+docker run -it -d --rm -p 80:80 epicstory-app
