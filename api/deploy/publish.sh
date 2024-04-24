271a596ea06f56987fa7360a10ed90b1316a6f4c
Mateus Sarmento <omateusmento@gmail.com>
2024-04-24 09:58:45 -0300

    Replace hardcoded values with args in publish.sh

--- api/deploy/publish.sh
+++ api/deploy/publish.sh
@@ -2,12 +2,12 @@
 
 
 # Creating AWS ECR repository
-aws ecr create-repository --repository-name epicstory-api --region sa-east-1
+aws ecr create-repository --repository-name epicstory-api --region ${AWS_REGION}
 
 # Login docker to AWS ECR
-aws ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin 429249706241.dkr.ecr.sa-east-1.amazonaws.com
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
 # Build and publish docker image
 docker build -t epicstory-api .
-docker tag epicstory-api 429249706241.dkr.ecr.sa-east-1.amazonaws.com/epicstory-api
-docker push 429249706241.dkr.ecr.sa-east-1.amazonaws.com/epicstory-api
+docker tag epicstory-api ${AWS_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com/epicstory-api
+docker push ${AWS_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com/epicstory-api
