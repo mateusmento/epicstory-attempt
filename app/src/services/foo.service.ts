016492bccd6fe49ccf40955d7f407b8c9111dcd7
Mateus Sarmento <omateusmento@gmail.com>
2024-04-24 09:47:52 -0300

    Add VITE_API_URL configuration for app

--- app/src/services/foo.service.ts
+++ app/src/services/foo.service.ts
@@ -2,11 +2,9 @@ import { InjectAxios } from '@/core/axios';
 import type { Axios } from 'axios';
 import { injectable } from 'tsyringe';
 
-const baseURL = 'http://localhost:3000';
-
 @injectable()
 export class FooService {
-  constructor(@InjectAxios({ baseURL }) private axios: Axios) {}
+  constructor(@InjectAxios({ baseURL: import.meta.env.VITE_API_URL }) private axios: Axios) {}
 
   getFoos() {
     return this.axios.get('/foos').then((res) => res.data);
