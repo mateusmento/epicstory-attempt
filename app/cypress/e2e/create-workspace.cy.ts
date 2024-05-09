b1e5d82b9772d43f40ced84d8c71db11019609fb
Mateus Sarmento <omateusmento@gmail.com>
2024-05-09 08:01:35 -0300

    Add prototype for creating workspace

--- app/cypress/e2e/create-workspace.cy.ts
+++ app/cypress/e2e/create-workspace.cy.ts
@@ -2,5 +2,8 @@ describe('Create workspace', () => {
   it('should create an workspace', () => {
     cy.visit('/');
     cy.contains('Workspaces');
+    cy.get('[data-testid="workspace-name-input"]').type('Epicstory');
+    cy.get('[data-testid="create-workspace-button"').click();
+    cy.get('[data-testid="workspace-list"]').contains('Epicstory');
   });
 });
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,7 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+interface Workspace {
+  id: number;
+  name: string;
+}
+
+const workspaces = ref<Workspace[]>([]);
+
+const workspace = reactive({
+  name: '',
+});
+
+function createWorkspace() {
+  const maxId = workspaces.value.reduce((maxId, w) => Math.max(maxId, w.id), 0) + 1;
+  workspaces.value.push({
+    id: maxId,
+    ...workspace,
+  });
+}
+</script>
 
 <template>
   <main>
     <h1>Workspaces</h1>
+    <form @submit.prevent="createWorkspace">
+      <h2>Create workspace</h2>
+      <label>Name</label>
+      <input v-model="workspace.name" data-testid="workspace-name-input" />
+      <button type="submit" data-testid="create-workspace-button">Create</button>
+    </form>
+    <ul data-testid="workspace-list">
+      <li v-for="workspace of workspaces" :key="workspace.id">{{ workspace.name }}</li>
+    </ul>
   </main>
 </template>
