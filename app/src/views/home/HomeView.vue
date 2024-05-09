97725259059d95347e0ce5366a0231644988dc25
Mateus Sarmento <omateusmento@gmail.com>
2024-05-09 09:21:01 -0300

    Use create workspace form in home view

--- app/src/views/home/HomeView.vue
+++ app/src/views/home/HomeView.vue
@@ -2,32 +2,23 @@
 import { useDependency } from '@/core/dependency-injection';
 import { WorkspaceService } from '@/services/workspace.service';
 import type { Workspace } from '@/types/workspace';
-import { reactive, ref } from 'vue';
+import { ref } from 'vue';
+import CreateWorkspaceForm from './CreateWorkspaceForm.vue';
 
 const workspaceService = useDependency(WorkspaceService);
 
 const workspaces = ref<Workspace[]>([]);
 
-const formData = reactive({
-  name: '',
-});
-
-async function createWorkspace() {
+async function createWorkspace(formData: { name: string }) {
   const workspace = await workspaceService.createWorkspace(formData.name).then((res) => res.data);
   workspaces.value.push(workspace);
-  formData.name = '';
 }
 </script>
 
 <template>
   <main>
     <h1>Workspaces</h1>
-    <form @submit.prevent="createWorkspace">
-      <h2>Create workspace</h2>
-      <label>Name</label>
-      <input v-model="formData.name" data-testid="workspace-name-input" />
-      <button type="submit" data-testid="create-workspace-button">Create</button>
-    </form>
+    <CreateWorkspaceForm @submit="createWorkspace" />
     <ul data-testid="workspace-list">
       <li v-for="workspace of workspaces" :key="workspace.id">{{ workspace.name }}</li>
     </ul>
