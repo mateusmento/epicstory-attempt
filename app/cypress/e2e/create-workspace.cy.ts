9470c0ad794f026fa16e1d88ba64156f1ebf155a
Mateus Sarmento <omateusmento@gmail.com>
2024-05-09 09:03:42 -0300

    Intercept POST /workspaces request in create workspace test

--- app/cypress/e2e/create-workspace.cy.ts
+++ app/cypress/e2e/create-workspace.cy.ts
@@ -1,5 +1,12 @@
+const API_URL = 'http://localhost:3000';
+
 describe('Create workspace', () => {
   it('should create an workspace', () => {
+    let idCounter = 0;
+    cy.intercept('POST', `${API_URL}/workspaces`, (req) => {
+      req.reply({ id: idCounter++, name: req.body.name });
+    });
+
     cy.visit('/');
     cy.contains('Workspaces');
     cy.get('[data-testid="workspace-name-input"]').type('Epicstory');
