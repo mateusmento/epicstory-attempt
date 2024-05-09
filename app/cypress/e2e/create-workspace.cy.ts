1c7f3b486faa538919ca3424a7e95be2effd419d
Mateus Sarmento <omateusmento@gmail.com>
2024-05-09 09:27:38 -0300

    Remove duplicated steps

--- app/cypress/e2e/create-workspace.cy.ts
+++ app/cypress/e2e/create-workspace.cy.ts
@@ -2,24 +2,26 @@ const API_URL = 'http://localhost:3000';
 
 describe('Create workspace', () => {
   it('should create an workspace', () => {
-    let idCounter = 0;
+    let counter = 0;
     cy.intercept('POST', `${API_URL}/workspaces`, (req) => {
-      req.reply({ id: idCounter++, name: req.body.name });
+      req.reply({ id: counter++, name: req.body.name });
     });
+
+    cy.visit('/');
+    cy.contains('Workspaces');
 
     const workspaceNameInput = () => cy.get('[data-testid="workspace-name-input"]');
     const createWorkspaceButton = () => cy.get('[data-testid="create-workspace-button"');
     const workspaceList = () => cy.get('[data-testid="workspace-list"]');
 
-    cy.visit('/');
-    cy.contains('Workspaces');
-    workspaceNameInput().type('Epicstory');
-    createWorkspaceButton().click();
-    workspaceList().contains('Epicstory');
-    workspaceNameInput().should('have.value', '');
-    workspaceNameInput().type('Derbel');
-    createWorkspaceButton().click();
-    workspaceList().contains('Derbel');
-    workspaceNameInput().should('have.value', '');
+    const createWorkspace = (name: string) => {
+      workspaceNameInput().type(name);
+      createWorkspaceButton().click();
+      workspaceList().contains(name);
+      workspaceNameInput().should('have.value', '');
+    };
+
+    createWorkspace('Epicstory');
+    createWorkspace('Derbel');
   });
 });
