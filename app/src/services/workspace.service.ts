a36bd833f866b34f134bc2a86ff677061bd78b42
Mateus Sarmento <omateusmento@gmail.com>
2024-05-09 09:09:22 -0300

    Use workspace type in workspace service

--- app/src/services/workspace.service.ts
+++ app/src/services/workspace.service.ts
@@ -1,4 +1,5 @@
 import { InjectAxios } from '@/core/axios';
+import type { Workspace } from '@/types/workspace';
 import type { Axios } from 'axios';
 import { injectable } from 'tsyringe';
 
@@ -7,6 +8,6 @@ export class WorkspaceService {
   constructor(@InjectAxios() private axios: Axios) {}
 
   createWorkspace(name: string) {
-    return this.axios.post('/workspaces', { name });
+    return this.axios.post<Workspace>('/workspaces', { name });
   }
 }
