fbcc9fb44d6f7e3f6c29dc64268f9f52b18c18b6
Mateus Sarmento <omateusmento@gmail.com>
2024-05-09 21:51:13 -0300

    Add project service

--- app/src/app/dependencies.ts
+++ app/src/app/dependencies.ts
@@ -1,5 +1,6 @@
 import { createAxios } from '@/core/axios';
 import { FooService } from '@/services/foo.service';
+import { ProjectService } from '@/services/project.service';
 import { WorkspaceService } from '@/services/workspace.service';
 import { Axios } from 'axios';
 import { container as tsyringe } from 'tsyringe';
@@ -11,5 +12,6 @@ export default async function createDependencies() {
   container.registerInstance(Axios, axios);
   container.registerSingleton(FooService);
   container.registerSingleton(WorkspaceService);
+  container.registerSingleton(ProjectService);
   return container;
 }
---
+++ app/src/services/project.service.ts
@@ -0,0 +1,15 @@
+import { InjectAxios } from '@/core/axios';
+import type { Project } from '@/types/project';
+import type { Axios } from 'axios';
+import { injectable } from 'tsyringe';
+
+@injectable()
+export class ProjectService {
+  constructor(@InjectAxios() private axios: Axios) {}
+
+  createProject(workspaceId: number, name: string) {
+    return this.axios
+      .post<Project>(`/workspaces/${workspaceId}/projects`, { name })
+      .then((res) => res.data);
+  }
+}
