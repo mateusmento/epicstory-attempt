2d55289d8246250ea7d81a085624d072c9862161
Mateus Sarmento <omateusmento@gmail.com>
2024-05-09 21:42:41 -0300

    Add link for workspace view in workspace list

--- app/src/views/home/WorkspaceList.vue
+++ app/src/views/home/WorkspaceList.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Workspace } from '@/types/workspace';
+import { RouterLink } from 'vue-router';
 
 defineProps<{
   workspaces: Workspace[];
@@ -8,6 +9,8 @@ defineProps<{
 
 <template>
   <ul data-testid="workspace-list">
-    <li v-for="workspace of workspaces" :key="workspace.id">{{ workspace.name }}</li>
+    <li v-for="workspace of workspaces" :key="workspace.id">
+      <RouterLink :to="`/workspace/${workspace.id}`">{{ workspace.name }}</RouterLink>
+    </li>
   </ul>
 </template>
