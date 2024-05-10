8ca61e5d17c29d3511b3b5ba5bcf621a4d5fbe3f
Mateus Sarmento <omateusmento@gmail.com>
2024-05-10 07:16:10 -0300

    Remove deep scope in create issue test

--- app/cypress/e2e/create-issue-in-project.cy.ts
+++ app/cypress/e2e/create-issue-in-project.cy.ts
@@ -2,23 +2,17 @@ describe('Create issue in project', () => {
   const API_URL = 'http://localhost:3000';
 
   it('should create issue in project', () => {
-    {
-      cy.intercept('POST', `${API_URL}/workspaces`, (req) => {
-        req.reply({ id: 1, name: req.body.name });
-      });
-    }
+    cy.intercept('POST', `${API_URL}/workspaces`, (req) => {
+      req.reply({ id: 1, name: req.body.name });
+    });
 
-    {
-      cy.intercept('POST', `${API_URL}/workspaces/1/projects`, (req) => {
-        req.reply({ id: 1, workspaceId: 1, name: req.body.name });
-      });
-    }
+    cy.intercept('POST', `${API_URL}/workspaces/1/projects`, (req) => {
+      req.reply({ id: 1, workspaceId: 1, name: req.body.name });
+    });
 
-    {
-      cy.intercept('POST', `${API_URL}/projects/1/issues`, (req) => {
-        req.reply({ id: 1, projectId: 1, title: req.body.title });
-      });
-    }
+    cy.intercept('POST', `${API_URL}/projects/1/issues`, (req) => {
+      req.reply({ id: 1, projectId: 1, title: req.body.title });
+    });
 
     cy.visit('/');
 
