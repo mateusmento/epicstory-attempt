b3517d7a626b4aea46582802e89ba9d4d2f399d4
Mateus Sarmento <omateusmento@gmail.com>
2024-05-10 07:14:42 -0300

    Returns issue type in create issue of issue service

--- app/src/services/issue.service.ts
+++ app/src/services/issue.service.ts
@@ -1,4 +1,5 @@
 import { InjectAxios } from '@/core/axios';
+import type { Issue } from '@/types/issue';
 import type { Axios } from 'axios';
 import { injectable } from 'tsyringe';
 
@@ -7,6 +8,8 @@ export class IssueService {
   constructor(@InjectAxios() private axios: Axios) {}
 
   createIssue(projectId: number, title: string) {
-    return this.axios.post(`/projects/${projectId}/issues`, { title }).then((res) => res.data);
+    return this.axios
+      .post<Issue>(`/projects/${projectId}/issues`, { title })
+      .then((res) => res.data);
   }
 }
