fe558517f2522ad120d581fc66904187da8a3a31
Mateus Sarmento <mateus.sarmento@shawandpartners.com>
2024-05-30 21:20:38 -0300

    Add drag and drop story

---
+++ app/src/components/DragAndDropArea.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+function grabFile(x: MouseEvent) {
+  x.preventDefault();
+  console.log(x);
+}
+</script>
+
+<template>
+  <div class="drag-and-drop-area" @dragover="(e) => e.preventDefault()" @drop="grabFile"></div>
+</template>
+
+<style>
+.drag-and-drop-area {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 400px;
+  height: 300px;
+  border: 2px dashed #222;
+}
+
+.drag-and-drop-area::before {
+  content: 'Drag a file here';
+}
+
+.drag-and-drop-area:-moz-drag-over::before {
+  content: 'Drop your file';
+}
+</style>
